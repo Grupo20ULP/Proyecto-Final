9cd7637c4ed10165a4d53311735464f51ccc7765
Federico Galan <tumachgalan14@gmail.com>
2025-10-20 20:39:17 -0300

    Conexion BD

--- SpaEntreDedos/src/Persistencia/Conexion.java
+++ SpaEntreDedos/src/Persistencia/Conexion.java
@@ -1,9 +1,38 @@
 package Persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author Heber Gomez
+ * @author Federico Galan
  */
 public class Conexion {
-
+    private static final String URL = "jdbc:mariadb://localhost:3306/";
+    private static final String DB = "gp20uni";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = "";
+    private static Connection conexion;
+    
+    
+    public static Connection getConectar () {
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = (Connection) DriverManager.getConnection(URL + DB,
+                    USUARIO, CLAVE);               
+            }
+            catch (ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null,
+                    "Problemas accediendo a la clase " + e.getMessage());
+            }
+            catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,
+                    "Error al conectar a la base de datos " + ex.getMessage());
+            }
+        }
+        return conexion;
+    }
 }
