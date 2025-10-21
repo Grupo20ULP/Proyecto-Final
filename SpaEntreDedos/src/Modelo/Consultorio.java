eff4f2b49416e37989fdb0428297a9557ac847de
NehuenZ12 <aldonehuen123@gmail.com>
2025-10-21 02:44:00 -0300

    Clase consultorio

--- SpaEntreDedos/src/Modelo/Consultorio.java
+++ SpaEntreDedos/src/Modelo/Consultorio.java
@@ -2,8 +2,68 @@ package Modelo;
 
 /**
  *
- * @author Heber Gomez
+ * @author Nehuen Zerdá
  */
 public class Consultorio {
+    
+    // --- Atributos ---
+    private int nroConsultorio;
+    private String usos;          // facial, corporal, relajacion o estetico
+    private String equipamiento;  // descripción del equipamiento disponible
+    private String apto;          // "si" o "no"
+    
+    
+    // --- Constructores ---
+    
+    // Constructor vacío (necesario para instancias sin datos iniciales)
+    public Consultorio() {
+    }
+
+    // Constructor con todos los campos (útil para crear objetos directamente)
+    public Consultorio(int nroConsultorio, String usos, String equipamiento, String apto) {
+        this.nroConsultorio = nroConsultorio;
+        this.usos = usos;
+        this.equipamiento = equipamiento;
+        this.apto = apto;
+    }
+    
+    
+    public int getNroConsultorio() {
+        return nroConsultorio;
+    }
+
+    public void setNroConsultorio(int nroConsultorio) {
+        this.nroConsultorio = nroConsultorio;
+    }
+
+    public String getUsos() {
+        return usos;
+    }
+
+    public void setUsos(String usos) {
+        this.usos = usos;
+    }
+
+    public String getEquipamiento() {
+        return equipamiento;
+    }
+
+    public void setEquipamiento(String equipamiento) {
+        this.equipamiento = equipamiento;
+    }
+
+    public String getApto() {
+        return apto;
+    }
+
+    public void setApto(String apto) {
+        this.apto = apto;
+    }
+   
+    // --- Métodos Utiles ---
+    @Override
+    public String toString() {
+        return "Consultorio Nº " + nroConsultorio + " (" + usos + ")";
+    }
 
 }
