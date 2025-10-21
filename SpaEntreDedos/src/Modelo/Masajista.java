535029598e97de9606528067ca2db392d1b64c98
Heber Gomez <heber12398@gmail.com>
2025-10-20 21:05:12 -0300

    Clase Masajista y Tratamiento terminado
    cualquier correccion que se necesite hacer hablar al grupo de Whatsapp

--- SpaEntreDedos/src/Modelo/Masajista.java
+++ SpaEntreDedos/src/Modelo/Masajista.java
@@ -6,4 +6,74 @@ package Modelo;
  */
 public class Masajista {
 
+    private int matricula;
+    private String nombreYapellido;
+    private int telefono;
+    private String especialidad;
+    private boolean activo;
+
+    public Masajista () {
+    }
+
+    public Masajista (String nombreYapellido, int telefono, String especialidad, boolean activo) {
+        this.nombreYapellido = nombreYapellido;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+        this.activo = activo;
+    }
+
+    public Masajista (int matricula, String nombreYapellido, int telefono, String especialidad, boolean activo) {
+        this.matricula = matricula;
+        this.nombreYapellido = nombreYapellido;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+        this.activo = activo;
+    }
+
+    public int getMatricula () {
+        return matricula;
+    }
+
+    public void setMatricula (int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombreYapellido () {
+        return nombreYapellido;
+    }
+
+    public void setNombreYapellido (String nombreYapellido) {
+        this.nombreYapellido = nombreYapellido;
+    }
+
+    public int getTelefono () {
+        return telefono;
+    }
+
+    public void setTelefono (int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEspecialidad () {
+        return especialidad;
+    }
+
+    public void setEspecialidad (String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isActivo () {
+        return activo;
+    }
+
+    public void setActivo (boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString () {
+        return "Masajista{" + "matricula=" + matricula + ", nombreYapellido="
+            + nombreYapellido + ", telefono=" + telefono + ", especialidad="
+            + especialidad + ", activo=" + activo + '}';
+    }
 }
--- SpaEntreDedos/src/Modelo/Tratamiento.java
+++ SpaEntreDedos/src/Modelo/Tratamiento.java
@@ -6,4 +6,96 @@ package Modelo;
  */
 public class Tratamiento {
 
+    private int codTratam;
+    private String nombre;
+    private String tipo;
+    private String detalle;
+    private double duracion;
+    private double costo;
+    private boolean activo;
+
+    public Tratamiento () {
+    }
+
+    public Tratamiento (String nombre, String tipo, String detalle, double duracion, double costo, boolean activo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.detalle = detalle;
+        this.duracion = duracion;
+        this.costo = costo;
+        this.activo = activo;
+    }
+
+    public Tratamiento (int codTratam, String nombre, String tipo, String detalle, double duracion, double costo, boolean activo) {
+        this.codTratam = codTratam;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.detalle = detalle;
+        this.duracion = duracion;
+        this.costo = costo;
+        this.activo = activo;
+    }
+
+    public int getCodTratam () {
+        return codTratam;
+    }
+
+    public void setCodTratam (int codTratam) {
+        this.codTratam = codTratam;
+    }
+
+    public String getNombre () {
+        return nombre;
+    }
+
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo () {
+        return tipo;
+    }
+
+    public void setTipo (String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDetalle () {
+        return detalle;
+    }
+
+    public void setDetalle (String detalle) {
+        this.detalle = detalle;
+    }
+
+    public double getDuracion () {
+        return duracion;
+    }
+
+    public void setDuracion (double duracion) {
+        this.duracion = duracion;
+    }
+
+    public double getCosto () {
+        return costo;
+    }
+
+    public void setCosto (double costo) {
+        this.costo = costo;
+    }
+
+    public boolean isActivo () {
+        return activo;
+    }
+
+    public void setActivo (boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString () {
+        return "Tratamiento{" + "codTratam=" + codTratam + ", nombre=" + nombre
+            + ", tipo=" + tipo + ", detalle=" + detalle + ", duracion="
+            + duracion + ", costo=" + costo + ", activo=" + activo + '}';
+    }
 }
